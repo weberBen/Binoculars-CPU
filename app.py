a99586107df7e133d3727bca213691705e36b592
bweber <bweber@hotmail.fr>
2024-12-11 22:44:42 +0100

    fix(demo): disable gradio API

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from demo.utils import MAX_FILE_SIZE
 
 if __name__ == "__main__":
     # Launch the Gradio interface
-    app.launch(show_api=True, debug=True, share=False, max_file_size=MAX_FILE_SIZE)
+    app.launch(show_api=False, debug=True, share=False, max_file_size=MAX_FILE_SIZE)
